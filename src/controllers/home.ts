548a6516b447d06f564a7dd6b83a19958803ffcd
Opesanya Adebayo <bayoopesanya@gmail.com>
2018-07-27 00:38:32 +0100

    added validation for signup routes

--- src/controllers/home.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from 'express';
-import { NextFunction } from 'connect';
-
-export let index = (req: Request, res: Response, next: NextFunction) => {
-    res.sendStatus(200);
-}
---
+++ src/utils/validator.ts
@@ -0,0 +1,5 @@
+import { body } from 'express-validator/check';
+export const checkSignup = [
+    body("email").isEmail().withMessage("Email is invalid"),
+    body("password").isLength({ min: 4 }).withMessage("Password must be 4 characters or more")
+]
