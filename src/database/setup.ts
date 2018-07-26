cc780fdf929c4124be183ddc9ef2b0e0b43ef4f2
Opesanya Adebayo <bayoopesanya@gmail.com>
2018-07-27 00:35:49 +0100

    redis client setup

--- src/database/setup.ts
+++ src/database/setup.ts
@@ -1,3 +1,9 @@
 import redis from "redis";
 export const RedisClient = redis.createClient();
 
+RedisClient.on("connect", () => {
+  console.log("Redis client connected");
+});
+RedisClient.on("error", function(err) {
+  console.log("Something went wrong " + err);
+});
