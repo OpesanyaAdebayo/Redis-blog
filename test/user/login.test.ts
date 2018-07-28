f3c6e9e21b0b0feff4132f99e4a1805349cf90aa
Opesanya Adebayo <bayoopesanya@gmail.com>
2018-07-28 21:20:32 +0100

    update login tests

---
+++ test/user/login.test.ts
@@ -0,0 +1,46 @@
+import request from "supertest";
+import app from "../../src/app";
+
+describe("GET /login", () => {
+    it("should return 200 OK", (done) => {
+        return request(app).get("/login")
+            .expect(200, done);
+    });
+});
+
+describe("POST /login", () => {
+    it("should give an error if invalid email is supplied", (done) => {
+        return request(app)
+            .post("/login")
+            .send({ email: "meetme.com" })
+            .send({ password: "zbgcc" })
+            .expect(401)
+            .end(function (err, res) {
+                expect(res.body.error).not.toBe(undefined);
+                done();
+            });
+    });
+
+    it("should return error when password is less than 4 characters.", done => {
+        return request(app)
+            .post("/login")
+            .send({ email: "meetme@xyz.com" })
+            .send({ password: "dz" })
+            .expect(401)
+            .end(function (err, res) {
+                expect(res.body.error).not.toBe(undefined);
+                done();
+            });
+    });
+    it("should return some defined error message with valid parameters", (done) => {
+        return request(app).post("/login")
+            .send({ email: "meet@me.com" })
+            .send({ password: "zcqwpo9" })
+            .expect(401)
+            .end(function (err, res) {
+                expect(res.body.error).not.toBe(undefined)
+                done();
+            });
+
+    });
+});
