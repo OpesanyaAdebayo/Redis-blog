4d301da7a9e51ff139010b96dda17093027f9c84
Opesanya Adebayo <bayoopesanya@gmail.com>
2018-07-28 17:10:58 +0100

    add mongodb url

--- src/utils/secrets.ts
+++ src/utils/secrets.ts
@@ -7,5 +7,15 @@ dotenv.load();
 
 const parsed = env.parsed! || process.env;
 
-export const SESSION_SECRET =
-  parsed["SESSION_SECRET"] || process.env.SESSION_SECRET!;
+export const SESSION_SECRET = parsed["SESSION_SECRET"] || process.env.SESSION_SECRET!;
+export const MLAB_URI = parsed["MLAB_URI"] || process.env.MLAB_URI!;
+
+if (!SESSION_SECRET) {
+    console.error("No client secret. Set SESSION_SECRET environment variable.");
+    process.exit(1);
+}
+
+if (!MLAB_URI) {
+    console.error("No mongo connection string. Set MLAB_URI environment variable.");
+    process.exit(1);
+}
