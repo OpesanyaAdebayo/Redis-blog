bfb1b99d9e3851f7566fdf264e7acdee8c355ab9
Opesanya Adebayo <bayoopesanya@gmail.com>
2018-07-28 21:21:14 +0100

    update validator for login

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -3,3 +3,7 @@ export const checkSignup = [
     body("email").isEmail().withMessage("Email is invalid"),
     body("password").isLength({ min: 4 }).withMessage("Password must be 4 characters or more")
 ]
+export const checkLogin = [
+    body("email").isEmail().withMessage("Email is invalid"),
+    body("password").isLength({ min: 4 }).withMessage("Password must be 4 characters or more")
+]
