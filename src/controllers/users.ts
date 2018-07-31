440b2d7356b841958b5f9ca7750d5a30605ec30f
Opesanya Adebayo <bayoopesanya@gmail.com>
2018-07-31 17:36:56 +0100

    clean up routes

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -6,7 +6,7 @@ import redis from 'redis';
 
 import { RedisClient, saveUser } from '../database/redis';
 import { default as User, userType } from '../models/User';
-export let getHome = (req: Request, res: Response, next: NextFunction) => {
+export let getHome = (req: Request, res: Response) => {
     if (req.session!.userID) {
         return res
             .status(302)
@@ -42,7 +42,7 @@ export let getSignup = (req: Request, res: Response) => {
         message: "On the signup page"
     });
 };
-export let getLogOut = (req: Request, res: Response, next: NextFunction) => {
+export let getLogOut = (req: Request, res: Response) => {
     if (req.session!.userID) {
         return req.session!.destroy((err) => {
             return res
@@ -58,49 +58,60 @@ export let getLogOut = (req: Request, res: Response, next: NextFunction) => {
 };
 
 export let postLogin = (req: Request, res: Response, next: NextFunction) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(401).json({
-      error: errors.array()
-    });
-  }
-
-  const user = new User({
-    email: req.body.email,
-    password: req.body.password
-  });
-
-  User.findOne({ email: req.body.email }, (err, existingUser: any) => {
-    if (err) {
-      return next(err);
+    if (req.session!.userID) {
+        return res
+            .status(302)
+            .json({
+                message: "Redirected to user dashboard"
+            });
     }
-    if (existingUser) {
-      existingUser.comparePassword(
-        req.body.password,
-        (err: Error, isMatch: boolean) => {
-          if (err) {
-            return next(err);
-          }
-          if (isMatch) {
-            req.session!.userID = existingUser._id.toString();
-            return res
-              .status(302)
-              .json({ message: "redirected to user dashboard" });
-          } else {
-            return res
-              .status(401)
-              .json({ error: "Incorrect username and/or password." });
-          }
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(401).json({
+            error: errors.array()
+        });
+    }
+
+    User.findOne({ email: req.body.email }, (err, existingUser: any) => {
+        if (err) {
+        return next(err);
         }
-      );
-    } else {
-      return res
-        .status(401)
-        .json({ error: "Incorrect username and/or password." });
-    }
-  });
+        if (existingUser) {
+        existingUser.comparePassword(
+            req.body.password,
+            (err: Error, isMatch: boolean) => {
+            if (err) {
+                return next(err);
+            }
+            if (isMatch) {
+                req.session!.userID = existingUser._id.toString();
+                return res
+                .status(302)
+                .json({ message: "redirected to user dashboard" });
+            } else {
+                return res
+                .status(401)
+                .json({ error: "Incorrect username and/or password." });
+            }
+            }
+        );
+        } else {
+        return res
+            .status(401)
+            .json({ error: "Incorrect username and/or password." });
+        }
+    });
 };
 export let postSignup = (req: Request, res: Response, next: NextFunction) => {
+    if (req.session!.userID) {
+      return res
+        .status(302)
+        .json({
+          message: "Redirected to user dashboard"
+        });
+    }
+    
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
